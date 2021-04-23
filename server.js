28c7b1cd449792bae3e2b146fd7588cdd10afdfc
raihanuddin561 <raihanuddin561@gmail.com>
2021-04-23 14:59:53 +0600

    ready for deployment

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const connectDB = require('./config/db')
 const app = express()
+const path = require('path')
 
 
 connectDB()
 app.use(express.json({extended:false}));
-app.get('/',(req,res)=>res.send('hello raihan'))
 app.use('/api/user',require('./routes/api/user'))
 app.use('/api/auth',require('./routes/api/auth'))
 app.use('/api/posts',require('./routes/api/posts'))
 app.use('/api/profile',require('./routes/api/profile'))
+
+//serve static assets to production
+if(process.env.NODE_ENV='production'){
+    app.use(express.static('client/build'))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve('client','build','index.html'))
+    })
+}
+
+
+
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT,()=>console.log(`Server started on port ${PORT}`))
